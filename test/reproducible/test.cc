df07cb6eb81afd71609f937a63b17e17f35920d4
Dan Schatzberg <schatzberg.dan@gmail.com>
2013-04-08 16:26:50 -0400

    Improved testing

--- test/reproducible/test.cc
+++ test/reproducible/test.cc
@@ -3,48 +3,51 @@
 #include <random>
 
 #include <boost/coroutine/all.hpp>
+#include <boost/program_options.hpp>
 
 #include "Coroutine.h"
 
 typedef boost::coroutines::coroutine<void()> coroutine_t;
 
+namespace po = boost::program_options;
+
 int main (int argc, char* argv[])
 {
-  if (argc < 6) {
-    std::cerr << "Usage: " << argv[0] << " num_processes num_messages"
-      " propose_probability failure_probability seed" << std::endl;
-    return -1;
+  bool verbose;
+  int num_processes;
+  int num_messages;
+  double propose_probability;
+  double failure_probability;
+  int seed;
+
+  po::options_description desc("Options");
+  try {
+    desc.add_options()
+      ("help", "produce help message")
+      ("v", po::value<bool>(&verbose)->default_value(false), "enable verbose output")
+      ("np", po::value<int>(&num_processes)->required(), "set number of processes")
+      ("nm", po::value<int>(&num_messages)->required(), "set number of messages")
+      ("p-prop", po::value<double>(&propose_probability)->required(),
+       "set proposal probability")
+      ("p-fail", po::value<double>(&failure_probability)->required(),
+       "set failure probability")
+      ("seed", po::value<int>(&seed)->required(), "set random number seed")
+      ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    if (vm.count("help")) {
+      std::cout << desc << std::endl;
+      return 1;
+    }
+
+    po::notify(vm);
+  } catch (std::exception& e) {
+    std::cout << desc << std::endl;
+    std::cout << e.what() << std::endl;
+    return 1;
   }
 
-  long int num_processes = strtol(argv[1], NULL, 10);
-  if (num_processes <= 0) {
-    std::cerr << "Invalid number of processes" << std::endl;
-    return -1;
-  }
-
-  long int num_messages = strtol(argv[2], NULL, 10);
-  if (num_messages <= 0) {
-    std::cerr << "Invalid number of messages" << std::endl;
-    return -1;
-  }
-
-  double propose_probability = strtod(argv[3], NULL);
-  if (propose_probability < 0 || propose_probability > 1) {
-    std::cerr << "Invalid propose probability" << std::endl;
-    return -1;
-  }
-
-  double failure_probability = strtod(argv[4], NULL);
-  if (failure_probability < 0 || failure_probability > 1) {
-    std::cerr << "Invalid failure probability" << std::endl;
-    return -1;
-  }
-
-  long int seed = strtol(argv[5], NULL, 10);
-  if (seed < 0) {
-    std::cerr << "Invalid seed" << std::endl;
-    return -1;
-  }
   std::default_random_engine rng(seed);
   std::vector<Coroutine*> coroutine_info(num_processes);
   std::set<Coroutine*> runnable;
